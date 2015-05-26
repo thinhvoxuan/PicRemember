1467c1fcc84c92ceedabc3a731e0a471ec2ec711
thinh voxuan <thinhvoxuan@gmail.com>
2015-05-26 16:38:43 +0700

    + layout main page

---
+++ Pic Remember iOS/Pic Remember/Pic Remember/Controller/ScoreViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ScoreViewController.swift
+//  Pic Remember
+//
+//  Created by thinhvoxuan on 5/26/15.
+//  Copyright (c) 2015 thinhvoxuan. All rights reserved.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
